4449e319b6d8dc813c0ac504c4efa0365c0f595f
swiftyy188 <jeffho.ml@gmail.com>
2016-09-06 15:38:43 -0400

    Added contact email

--- src/components/Shared/Contact.js
+++ src/components/Shared/Contact.js
@@ -6,7 +6,16 @@ class Contact extends Component {
       <div className="section contact" id="contacts">
         <h1>Contact</h1>
         <h2>Phone #: 123-456-7890</h2>
-        <h2>Email: JeffHoML@gmail.com</h2>
+        <h2>Email: hello@jeffmlho.com</h2>
+        <form action="https://formspree.io/hello@jeffmlho.com" method="POST">
+          <p>Name</p>
+          <input type="text" name="name"/>
+          <p>Email</p>
+          <input type="email" name="_replyto"/>
+          <p>Message</p>
+          <textarea name="message"></textarea>
+          <input type="submit" value="Send"/>
+        </form>
       </div>
     )
   }
